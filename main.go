ab8bca7c0d5a0a06e0be2756f9e154f96d8615f4
kthatoto <kthatoto@gmail.com>
2019-07-05 14:29:26 +0900

    Add consts

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+const (
+	N          = 10  // Ant and Antlion number
+	LOOP_COUNT = 100 // Max loop count
+	AREA_RANGE = 1   // Range where ants and antlions can move. When AREA_RANGE = 1 then ant can move in [-1, 1]
+	DIMENSION  = 3   // Ant and Antlion dimension count and also the number of arguments of the target function
+)
+
 func main() {
-	fmt.Println("123")
+	fmt.Println(LOOP_COUNT + AREA_RANGE)
 }
